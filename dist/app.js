83a17f66a4d529e15d9630535136d174d24d86ef
Pamal Mangat <mangatpamal@gmail.com>
2021-05-18 13:58:50 -0700

    Random APOD Function Works
    - Function implementation successfully fetches APOD for random date.

--- dist/app.js
+++ dist/app.js
@@ -28,7 +28,26 @@ const app = Vue.createApp({
 
     // Function to get random APOD for any previous date.
     async randomApod() {
-      alert('random');
+      // Generate random date (start - upto but not including current date)
+      let today_date = new Date();
+      let start_date = new Date(2015, 0, 1);
+      let rand_date =  new Date(start_date.getTime() + Math.random() * (today_date.getTime() - start_date.getTime())).toISOString().slice(0,10);
+
+      // Get APOD for random date.
+      const xhr = new XMLHttpRequest();
+      let query_str = 'https://api.nasa.gov/planetary/apod?api_key=RXK0pf2PFdSNOqRzbIgLmBWNboZDWq766gYb2UQC&date=' + rand_date;
+      xhr.open('GET', query_str);
+      xhr.responseType = 'json';
+      xhr.send();
+
+      xhr.onload = () => {
+        const result = xhr.response;
+        this.title = result['title'];
+        this.date = result['date'];
+        this.desc = result['explanation'];
+        this.image = result['hdurl'];
+      };
+
     }
   }
 })
